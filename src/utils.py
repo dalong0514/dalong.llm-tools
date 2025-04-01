948c707a025449b12babf55e72220d9f73e88cef
dalong <dalong0514@126.com>
2025-04-01 20:28:04 +0800

    refactor

--- src/utils.py
+++ src/utils.py
@@ -38,17 +38,18 @@ def get_all_files_from_directory(directory_path, file_extension=None):
 
 
 def modify_text(text):
+    """处理文字的格式"""
     # 去 \n 是转 pdf 时启用
     # line = line.replace('\n', '')
     text = pangu.spacing_text(text)
-    new_text = text.replace(' "', '"')\
-        .replace('" ', '"')\
-        .replace('"', '「')\
-        .replace('"', '」')\
+    new_text = text.replace(' “', '“')\
+        .replace('” ', '”')\
+        .replace('“', '「')\
+        .replace('”', '」')\
         .replace('・', '·')\
         .replace(', ', '，')\
         .replace('。 ', '。')\
-        .replace(''', '\'')\
+        .replace('’', '\'')\
         .replace(': ', '：')\
         .replace(') ', '）')\
         .replace(' (', '（')\
@@ -71,15 +72,16 @@ def modify_text(text):
     return new_text
 
 def modify_text_en(line):
+    """处理文字的格式"""
     # 去 \n 是转 pdf 时启用
     # line = line.replace('\n', '')
     line = pangu.spacing_text(line)
-    new_line = line.replace('"', '"')\
-        .replace('"', '"')\
+    new_line = line.replace('“', '"')\
+        .replace('”', '"')\
         .replace('・', '·')\
         .replace('， ', '，')\
         .replace('。 ', '。')\
-        .replace(''', '\'')\
+        .replace('’', '\'')\
         .replace('  ', ' ')
     new_line = new_line.strip()
     new_line = re.sub(r'(?<=[\u4e00-\u9fa5])\s+(?=[\u4e00-\u9fa5])', '', new_line)
