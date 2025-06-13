ea30b0d52279d2573c2e8e5a1250c4f599e24dfe
dalong <dalong0514@126.com>
2025-06-13 11:46:47 +0800

    uv add

--- scripts/batch_create_readnotes.py
+++ scripts/batch_create_readnotes.py
@@ -3,10 +3,10 @@ import time
 
 def batch_create_readnotes():
     first_num = 1
-    num = 9
-    base_path = "/Users/Daglas/dalong.github/dalong.readnotes/20250101复制书籍"
-    book_name = "2025071Build_a_Large_Language_Model"
-    default_content = "Sebastian Raschka.(2025).2025071Build_a_Large_Language_Model.Manning Publications Co => 00"
+    num = 12
+    base_path = "/Users/Daglas/dalong.github/dalong.readnotes/20210101复制书籍"
+    book_name = "2021148The_Psychology_of_Personal_Constructs_PartA"
+    default_content = "George A. Kelly.(1992).2021148The_Psychology_of_Personal_Constructs_PartA.Routledge => 00"
     
     for i in range(0, num):
         # 格式化序号，确保是4位数字，前两位是批次号，后两位是01
