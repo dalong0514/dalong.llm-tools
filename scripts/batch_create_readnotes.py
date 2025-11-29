1fc09c89cf4aed269f4dc5d04e9a63b7c11ea75a
dalong <dalong0514@126.com>
2025-11-29 23:51:47 +0800

    refactor batch_create_readnotes

--- scripts/batch_create_readnotes.py
+++ scripts/batch_create_readnotes.py
@@ -1,33 +1,35 @@
 # -*- coding: utf-8 -*-
 import time
 
+
 def batch_create_readnotes():
-    first_num = 1
-    num = 11
+    first_num = 0
+    num = 25
     base_path = "/Users/Daglas/dalong.github/dalong.readnotes/20250101复制书籍"
-    book_name = "2025204敏捷整洁之道"
-    default_content = "Robert C. Martin.(2020).2025204敏捷整洁之道.(申健等译).人民邮电出版社 => xxxx"
+    book_name = "2025205软件设计的哲学"
+    default_content = "John Ousterhout.(2024).2025205软件设计的哲学.(茹炳晟等译).人民邮电出版社 => xxxx"
 
     for i in range(0, num):
         # 格式化序号，确保是4位数字，前两位是批次号，后两位是01
         formatted_num = f"{first_num + i:02}01"
         file_name = f"{book_name}{formatted_num}.md"
         file_path = base_path + "/" + book_name + "/" + file_name
-        
+
         # 创建文件并写入内容
-        with open(file_path, 'w', encoding='utf-8') as f:
+        with open(file_path, "w", encoding="utf-8") as f:
             f.write(default_content)
-            
+
         print(f"已创建文件: {file_name}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start_time = time.time()
-    print('waiting...\n')
+    print("waiting...\n")
     batch_create_readnotes()
     end_time = time.time()
     # 改进时间显示
     elapsed_time = end_time - start_time
     if elapsed_time < 60:
-        print(f'Time Used: {elapsed_time:.2f} seconds')
+        print(f"Time Used: {elapsed_time:.2f} seconds")
     else:
-        print(f'Time Used: {elapsed_time/60:.2f} minutes')
+        print(f"Time Used: {elapsed_time / 60:.2f} minutes")
